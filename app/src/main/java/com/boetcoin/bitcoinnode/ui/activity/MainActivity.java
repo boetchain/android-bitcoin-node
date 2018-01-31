1198b92d4acda8b13e22df2aa4c3cfe93e54ac5e
Ross Badenhorst <rossbadenhorst@Rosss-MacBook-Pro.local>
2018-01-31 17:48:41 +0200

    Find ip of peers from dns seeds

--- app/src/main/java/com/boetcoin/bitcoinnode/ui/activity/MainActivity.java
+++ app/src/main/java/com/boetcoin/bitcoinnode/ui/activity/MainActivity.java
@@ -1,9 +1,14 @@
 package com.boetcoin.bitcoinnode.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.boetcoin.bitcoinnode.App;
 import com.boetcoin.bitcoinnode.R;
+import com.boetcoin.bitcoinnode.model.Peer;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +16,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Peer> locallySavedPeers = Peer.listAll(Peer.class);
+        Log.i(App.TAG, "locallySavedPeers: " + locallySavedPeers.size());
+
+        if (locallySavedPeers.size() == 0) {
+            Peer.findByDnsSeeds(getResources().getStringArray(R.array.dns_seed_nodes));
+        }
     }
 }
